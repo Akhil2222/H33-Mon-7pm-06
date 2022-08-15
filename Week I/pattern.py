0ce739f463838c691ec7e3d967377544da3a2624
Akhil2222 <70588733+Akhil2222@users.noreply.github.com>
2022-08-15 16:21:41 -0500

    Delete Week I directory

--- Week I/pattern.py
+++
@@ -1,9 +0,0 @@
-x=10
-for i in range(x):
-    str = ''
-    spac = ''
-    for j in range(i):
-        str = str + '*'
-    for j in range(x-i):
-        spac = spac + ' '
-    print(spac + str + ' ' + str)
